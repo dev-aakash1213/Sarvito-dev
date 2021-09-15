f98cee16105c1ec088f23a3c81032d9bb634376b
Satwik Tiwari <tsatwik87@gmai.com>
2021-09-15 13:15:47 +0530

    satwik commit

---
+++ lib/Description.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:favorite_button/favorite_button.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * .6,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                        'https://degraceplumbing.com/wp-content/uploads/2016/02/NJ-plumber.jpg'),
+                    fit: BoxFit.cover)),
+          ),
+          Positioned(
+            left: 30,
+            top: 30 + MediaQuery.of(context).padding.top,
+            child: InkWell(
+              onTap: () {},
+              child: ClipOval(
+                child: Container(
+                  height: 42,
+                  width: 41,
+                  decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(.25),
+                        offset: Offset(0, 4),
+                        blurRadius: 8)
+                  ]),
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: MediaQuery.of(context).size.height * .5,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(.2),
+                        offset: Offset(0, -4),
+                        blurRadius: 8)
+                  ]),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 20, left: 30, right: 30),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            "Plumbing Service",
+                            style: TextStyle(
+                                fontSize: 36, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        FavoriteButton(
+                          valueChanged: (_isFavorite) {
+                            print('Is Favorite $_isFavorite)');
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 20, left: 30, right: 30),
+                    child: Row(
+                      children: [
+                        Text(
+                          'Description',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.normal),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          '- Very good Service',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.normal),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 15,
+                      left: 30,
+                      right: 30,
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 4, horizontal: 8),
+                      decoration: BoxDecoration(
+                          color: Color.fromRGBO(63, 200, 101, 1),
+                          borderRadius: BorderRadius.circular(5)),
+                      child: Text(
+                        'Services are verified by Saarvito',
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 30),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black.withOpacity(.07),
+                              offset: Offset(0, -3),
+                              blurRadius: 12)
+                        ]),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Service Cost',
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                              Text(
+                                '\u{20B9}${500}',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              )
+                            ],
+                          ),
+                        ),
+                        Material(
+                          color: Color(0xff0095FF),
+                          borderRadius: BorderRadius.circular(10),
+                          child: InkWell(
+                            onTap: () {
+                              print('Click');
+                            },
+                            borderRadius: BorderRadius.circular(10),
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 10, horizontal: 20),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Text(
+                                'Get it Now',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
